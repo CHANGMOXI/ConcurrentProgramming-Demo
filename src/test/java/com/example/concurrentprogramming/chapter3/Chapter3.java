cd148ab11e30679559fbcb31a5947379161ba5ac
javagit <2143204964@qq.com>
2022-11-20 16:24:53 +0800

    chapter 3

---
+++ src/test/java/com/example/concurrentprogramming/chapter3/Chapter3.java
@@ -0,0 +1,34 @@
+package com.example.concurrentprogramming.chapter3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author CZS
+ * @create 2022-11-19 23:23
+ **/
+@SpringBootTest
+public class Chapter3 {
+    @Test
+    void RandomTest() {
+        //创建一个默认种子的随机数生成器
+        Random random = new Random();
+        //输出10个0~5(包含0不包含5)之间的随机数
+        for (int i = 0; i < 5; i++) {
+            System.out.println(random.nextInt(5));
+        }
+    }
+
+    @Test
+    void ThreadLocalRandomTest() {
+        //获取一个随机数生成器
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        //输出10个0~5(包含0不包含5)之间的随机数
+        for (int i = 0; i < 5; i++) {
+            System.out.println(random.nextInt(5));
+        }
+    }
+}
